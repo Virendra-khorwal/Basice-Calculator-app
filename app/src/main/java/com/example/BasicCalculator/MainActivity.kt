eab7eef3a155980075d0c063f5b7bd11dca12f54
Virendra-Khorwal <virendrakhorwalvk@gmail.com>
2021-08-30 09:16:17 +0530

    multiply added

--- app/src/main/java/com/example/BasicCalculator/MainActivity.kt
+++ app/src/main/java/com/example/BasicCalculator/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var Input2 : EditText
     lateinit var Add : Button
     lateinit var Subtract : Button
+    lateinit var Multiply : Button
     lateinit var Divide : Button
     lateinit var Average : Button
     lateinit var remainder : Button
@@ -27,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         Add = findViewById(R.id.Add)
         Subtract = findViewById(R.id.Subtract)
+        Multiply = findViewById(R.id.Multiply)
         Divide = findViewById(R.id.Divide)
         Average = findViewById(R.id.Average)
         remainder = findViewById(R.id.Remainder)
@@ -52,6 +54,17 @@ class MainActivity : AppCompatActivity() {
         var a = Input1.text
         var b = Input2.text
         var c = a.toString().toDouble() - b.toString().toDouble()
+        output.setText(c.toString())
+    }
+
+    fun multiply(view : View){
+        Input1 = findViewById(R.id.Input1)
+        Input2 = findViewById(R.id.Input2)
+        output = findViewById(R.id.output)
+
+        var a = Input1.text
+        var b = Input2.text
+        var c = a.toString().toDouble() * b.toString().toDouble()
         output.setText(c.toString())
     }
 
